b03a79c999efd5c3b69983e5b954db73ac8bca11
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-09-20 17:30:26 -0500

    crated personal branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.j
+          Edit <code>src/App.js</code> and save to reload.jla;sdjf
         </p>
         <a
           className="App-link"
