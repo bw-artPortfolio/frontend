8fa4c895b5691e443ab55bfb923aa814d391930f
juarezfrench <markedfrench@gmail.com>
2019-09-26 16:15:46 -0500

    Added missing files - Entry, Entrys, Context folder

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import axios from 'axios';
+
+import Cards from './Cards';
+
+const LandingPage = () => {
+  const [People, setPeople] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://art-portfolio-backend.herokuapp.com/api/entries/')
+      .then(response => {
+        setPeople(response.data.entries);
+        console.log(response.data.entries);
+      }, [])
+
+      .catch(error => {
+        console.log('', error);
+      });
+  }, []);
+
+  return (
+    <div className='App'>
+      {/*<Header />*/}
+
+      <div>
+        {People.map(artists => (
+          <Cards
+            artistName={artists.artistName}
+            title={artists.title}
+            url={artists.url}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
