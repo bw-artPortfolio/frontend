ef3fe94b7f50bd6571918af16b8269042e28cf79
juarezfrench <markedfrench@gmail.com>
2019-09-30 11:01:57 -0500

    Tweaked styling.

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -3,26 +3,27 @@ import CartContext from "./contexts/CartContext";
 import Item from "./ShoppingCartItem";
 
 function ShoppingCart() {
-  const cart = useContext(CartContext);
-  const getCartTotal = () => {
-    return cart
-      .reduce((acc, value) => {
-        return acc + value.price;
-      }, 0)
-      .toFixed(2);
-  };
+  // const cart = useContext(CartContext);
+  // const getCartTotal = () => {
+  //   return cart
+  //     .reduce((acc, value) => {
+  //       return acc + value.price;
+  //     }, 0)
+  //     .toFixed(2);
+  // };
 
   return (
-    <div className="shopping-cart">
-      {cart.map(item => (
-        <Item key={item.id} {...item} />
-      ))}
+    // <div className="shopping-cart">
+    //   {cart.map(item => (
+    //     <Item key={item.id} {...item} />
+    //   ))}
+    <div className="shopping-cart__checkout">
+        <h1>Shopping Cart</h1>
 
-      <div className="shopping-cart__checkout">
-        <p>Total: ${getCartTotal()}</p>
+        {/* <p>Total: ${getCartTotal()}</p> */}
         <button>Checkout</button>
       </div>
-    </div>
+    // </div>
   );
 }
 export default ShoppingCart;
